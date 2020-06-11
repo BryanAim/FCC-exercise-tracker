f6eb2bc73c69310f375b8856f20c5c5489ef1728
Isale <isale.bryan@gmail.com>
2020-06-11 12:58:41 +0300

    get exercise log

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/api/exercise/add', (req, res) => {
   const description = req.body.description;
   const duration = req.body.duration;
   const date = req.body.date;
-  let userId
+  let userId;
 
   if (username===undefined || description === undefined || duration === undefined || date === undefined) {
     res.send("Required field(s) are missing.");
@@ -138,9 +138,71 @@ app.post('/api/exercise/add', (req, res) => {
   }
 });
 
+// read exercise route
 
+app.get('/api/exercise/:log', (req,res) => {
+  const username = req.query.username;
+  let from = req.query.from;
+  let to = req.query.to;
+  let limit = req.query.limit;
+  let userId;
+  const query = {};
 
+  if (username===undefined) {
+    res.send("Username is undefines");
 
+  } else if (username=== '') {
+    res.send("Username is blank")
+  } else if (username.length> 10) {
+    res.send("Username cannot be more than 10 characters");
+  } else if (from !== undefined &&isNaN(Date.parse(from))===true) {
+    res.send("From is not a valid date");
+  } else if (to !== undefined && isNaN(Date.parse(to))=== true) {
+    res.send("to is not a valid date")
+  } else if (limit !== undefined && isNaN(limit)=== true) {
+    res.send("Limit is not a valid number");
+  } else if (limit !== undefined && Number(limit) < 1) {
+    res.send("Limit must be greater than 0");
+  } else {
+    // find user for username
+    User.findOne({username}, (err, user)=> {
+      if (err) {
+        res.send("Error searching for username, try again");
+      } else if (!user) {
+        res.send("Username not found");
+      } else {
+        userId = user.id;
+        query.userId = userId;
+
+        if (from !== undefined) {
+          from = new Date(from) ;
+          query.date = {$gte: from}
+        }
+
+        if (to !== undefined) {
+          to = new Date(to);
+          to.setDate(to.getDate() + 1) //add 1 day to unclude date
+          query.date = {$lt: to}
+        }
+
+        if (limit !== undefined) {
+          limit = Number(limit);
+        }
+
+        Exercise.find(query).select('userId description date duration').limit(limit).exec((errExercise, exercises) => {
+          if (err) {
+            res.send("Error searching for exercises, try again");
+
+          } else if (!user) {
+            res.send("Exercises not found");
+          } else {
+            res.json(exercises)
+          }
+        });
+      }
+    });
+  }
+});
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
