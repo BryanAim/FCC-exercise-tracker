775c980ae21f9041171955e310dfc56e06b41aa6
Isale <isale.bryan@gmail.com>
2020-06-11 11:54:24 +0300

    remove not found middleware

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ app.get('/', (req, res) => {
 
 
 // Not found middleware
-app.use((req, res, next) => {
-  return next({status: 404, message: 'not found'})
-})
+// app.use((req, res, next) => {
+//   return next({status: 404, message: 'not found'})
+// })
 
 // Error Handling middleware
 app.use((err, req, res, next) => {
