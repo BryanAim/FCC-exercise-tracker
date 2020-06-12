c1181fec41976401c0bffb367d1c658016a1227d
Isale <isale.bryan@gmail.com>
2020-06-12 12:48:41 +0300

    log function to see all exercises, 404 page

--- server.js
+++ server.js
@@ -59,6 +59,15 @@ app.get('/api/exercise/users', userHandler.getAllUsers);
 // If the user tries to add exercises to a given username in the database:
 app.post('/api/exercise/add', userHandler.addExercise);
 
+// If the user wants to GET the full exercise record for a given username:
+app.get('/api/exercise/log', userHandler.getAllExercises)
+
+// If the user navigates to a non-existing page, we'll serve them a 404 page:
+app.use(function(req, res, next) {
+  res.sendFile(__dirname + "/views/404.html");
+})
+
+
 
 // Not found middleware
 // app.use((req, res, next) => {
@@ -66,91 +75,91 @@ app.post('/api/exercise/add', userHandler.addExercise);
 // })
 
 // Error Handling middleware
-app.use((err, req, res, next) => {
-  let errCode, errMessage
+// app.use((err, req, res, next) => {
+//   let errCode, errMessage
 
-  if (err.errors) {
-    // mongoose validation error
-    errCode = 400 // bad request
-    const keys = Object.keys(err.errors)
-    // report the first validation error
-    errMessage = err.errors[keys[0]].message
-  } else {
-    // generic or custom error
-    errCode = err.status || 500
-    errMessage = err.message || 'Internal Server Error'
-  }
-  res.status(errCode).type('txt')
-    .send(errMessage)
-})
+//   if (err.errors) {
+//     // mongoose validation error
+//     errCode = 400 // bad request
+//     const keys = Object.keys(err.errors)
+//     // report the first validation error
+//     errMessage = err.errors[keys[0]].message
+//   } else {
+//     // generic or custom error
+//     errCode = err.status || 500
+//     errMessage = err.message || 'Internal Server Error'
+//   }
+//   res.status(errCode).type('txt')
+//     .send(errMessage)
+// })
 
 // read exercise route
 
-app.get('/api/exercise/:log', (req,res) => {
-  const username = req.query.username;
-  let from = req.query.from;
-  let to = req.query.to;
-  let limit = req.query.limit;
-  let userId;
-  const query = {};
+// app.get('/api/exercise/:log', (req,res) => {
+//   const username = req.query.username;
+//   let from = req.query.from;
+//   let to = req.query.to;
+//   let limit = req.query.limit;
+//   let userId;
+//   const query = {};
 
-  if (username===undefined) {
-    res.send("Username is undefines");
+//   if (username===undefined) {
+//     res.send("Username is undefines");
 
-  } else if (username=== '') {
-    res.send("Username is blank")
-  } else if (username.length> 10) {
-    res.send("Username cannot be more than 10 characters");
-  } else if (from !== undefined &&isNaN(Date.parse(from))===true) {
-    res.send("From is not a valid date");
-  } else if (to !== undefined && isNaN(Date.parse(to))=== true) {
-    res.send("to is not a valid date")
-  } else if (limit !== undefined && isNaN(limit)=== true) {
-    res.send("Limit is not a valid number");
-  } else if (limit !== undefined && Number(limit) < 1) {
-    res.send("Limit must be greater than 0");
-  } else {
-    // find user for username
-    User.findOne({username}, (err, user)=> {
-      if (err) {
-        res.send("Error searching for username, try again");
-      } else if (!user) {
-        res.send("Username not found");
-      } else {
-        userId = user.id;
-        query.userId = userId;
+//   } else if (username=== '') {
+//     res.send("Username is blank")
+//   } else if (username.length> 10) {
+//     res.send("Username cannot be more than 10 characters");
+//   } else if (from !== undefined &&isNaN(Date.parse(from))===true) {
+//     res.send("From is not a valid date");
+//   } else if (to !== undefined && isNaN(Date.parse(to))=== true) {
+//     res.send("to is not a valid date")
+//   } else if (limit !== undefined && isNaN(limit)=== true) {
+//     res.send("Limit is not a valid number");
+//   } else if (limit !== undefined && Number(limit) < 1) {
+//     res.send("Limit must be greater than 0");
+//   } else {
+//     // find user for username
+//     User.findOne({username}, (err, user)=> {
+//       if (err) {
+//         res.send("Error searching for username, try again");
+//       } else if (!user) {
+//         res.send("Username not found");
+//       } else {
+//         userId = user.id;
+//         query.userId = userId;
 
-        if (from !== undefined) {
-          from = new Date(from) ;
-          query.date = {$gte: from}
-        }
+//         if (from !== undefined) {
+//           from = new Date(from) ;
+//           query.date = {$gte: from}
+//         }
 
-        if (to !== undefined) {
-          to = new Date(to);
-          to.setDate(to.getDate() + 1) //add 1 day to unclude date
-          query.date = {$lt: to}
-        }
+//         if (to !== undefined) {
+//           to = new Date(to);
+//           to.setDate(to.getDate() + 1) //add 1 day to unclude date
+//           query.date = {$lt: to}
+//         }
 
-        if (limit !== undefined) {
-          limit = Number(limit);
-        }
+//         if (limit !== undefined) {
+//           limit = Number(limit);
+//         }
 
-        Exercise.find(query).select('userId description date duration').limit(limit).exec((errExercise, exercises) => {
-          if (err) {
-            res.send("Error searching for exercises, try again");
+//         Exercise.find(query).select('userId description date duration').limit(limit).exec((errExercise, exercises) => {
+//           if (err) {
+//             res.send("Error searching for exercises, try again");
 
-          } else if (!user) {
-            res.send("Exercises not found");
-          } else {
-            res.json(exercises)
-          }
-        });
-      }
-    });
-  }
-});
+//           } else if (!user) {
+//             res.send("Exercises not found");
+//           } else {
+//             res.json(exercises)
+//           }
+//         });
+//       }
+//     });
+//   }
+// });
 
-
+// Finally, we'll make sure that our app is "alive" by making sure that it's listening for incoming requests:
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
